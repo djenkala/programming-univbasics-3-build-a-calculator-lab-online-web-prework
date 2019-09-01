ebb3b7d525dfba3c0fefde20ff62798a932274a4
Daniel Jenkala <djenkala@yahoo.com>
2019-09-01 23:19:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,33 +1,39 @@
 def addition(num1, num2)
   add = num1 + num2
+  puts add
   return add
 
 end
 
 def subtraction(num1, num2)
   subtract = num1 - num2
+  puts subtract
   return subtract
 
 end
 
 def division(num1, num2)
   divide = num1 / num2
+  puts divide
   return divide
 
 end
 
 def multiplication(num1, num2)
   multiply = num1 * num2
+  puts multiply
   return multiply
 
 end
 
 def modulo(num1, num2)
   modulu = num1 % num2
+  puts modulo
   return modulo
 
 end
 
 def square_root(num)
+  square_root
 
 end
